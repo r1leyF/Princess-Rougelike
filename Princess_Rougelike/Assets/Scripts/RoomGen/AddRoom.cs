448ed3cf0faf3ae1791fc1940094b2fe394d98ad
An-Nguyen1 <annguyen1351893109@gmail.com>
2022-05-18 11:14:31 -0700

    stuff

--- Princess_Rougelike/Assets/Scripts/RoomGen/AddRoom.cs
+++ Princess_Rougelike/Assets/Scripts/RoomGen/AddRoom.cs
@@ -1,18 +1,14 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AddRoom : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class AddRoom : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private RoomTemplates templates;
+
+	void Start(){
+
+		templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+		templates.rooms.Add(this.gameObject);
+	}
 }
--- Princess_Rougelike/Assets/Scripts/RoomGen/RoomTemplates.cs
+++ Princess_Rougelike/Assets/Scripts/RoomGen/RoomTemplates.cs
@@ -1,22 +1,33 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RoomTemplates : MonoBehaviour
-{
-    public GameObject[] bottomRooms;
-    public GameObject[] topRooms;
-    public GameObject[] leftRooms;
-    public GameObject[] rightRooms;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class RoomTemplates : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public GameObject[] bottomRooms;
+	public GameObject[] topRooms;
+	public GameObject[] leftRooms;
+	public GameObject[] rightRooms;
+
+	public GameObject closedRoom;
+
+	public List<GameObject> rooms;
+
+	public float waitTime;
+	private bool spawnedBoss;
+	public GameObject boss;
+
+	void Update(){
+
+		if(waitTime <= 0 && spawnedBoss == false){
+			for (int i = 0; i < rooms.Count; i++) {
+				if(i == rooms.Count-1){
+					Instantiate(boss, rooms[i].transform.position, Quaternion.identity);
+					spawnedBoss = true;
+				}
+			}
+		} else {
+			waitTime -= Time.deltaTime;
+		}
+	}
 }
