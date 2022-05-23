f7508d44e696a6e9648ceabff9754eb2415141d1
An-Nguyen1 <annguyen1351893109@gmail.com>
2022-05-22 19:26:00 -0700

    slime

--- Princess_Rougelike/Assets/Scripts/Enemies/Slim.cs
+++ Princess_Rougelike/Assets/Scripts/Enemies/Slim.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Slim : MonoBehaviour
 {
@@ -10,24 +11,61 @@ public class Slim : MonoBehaviour
 
     //gameobject
     public GameObject floorSlime;
-
+    public NavMeshAgent agent;
+    GameManager manager;
 
     // Start is called before the first frame update
     void Start()
     {
+        manager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
 
+    // Update is called once per frame
+    public float wanderRadius;
+    public float wanderTimer;
+
+    private Transform target;
+    private float timer;
+
+    // Use this for initialization
+    void OnEnable()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        timer = wanderTimer;
     }
 
     // Update is called once per frame
     void Update()
     {
-        coolDown = coolDown - Time.deltaTime;
-        if (coolDown <= 0)
+        timer += Time.deltaTime;
+
+        if (timer >= wanderTimer)
         {
-            //Movment();
-            Instantiate(floorSlime, transform.position, Quaternion.identity);
-            coolDown = 1;
+            Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+            agent.SetDestination(newPos);
+            timer = 0;
         }
     }
+    void OnCollisionEnter(Collision otherObj)
+    {
+        Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+        agent.SetDestination(newPos);
+        if (otherObj.gameObject.tag == "Player" && manager.gameRunning)
+        {
+            otherObj.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.forward * 5, ForceMode.Impulse);
+            GetComponent<Enemy>().attack();
+        }
+    }
+    public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
+    {
+        Vector3 randDirection = Random.insideUnitSphere * dist;
 
+        randDirection += origin;
+
+        NavMeshHit navHit;
+
+        NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
+
+        return navHit.position;
+    }
 }
